97e13d4ca64220463369c1fc8b896b6e6b72cd60
Thomas Van Meervenne <vanmi46@gmail.com>
2026-02-13 13:09:27 +0100

    partie 3

---
+++ src/Form/TaskType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
+class TaskType extends AbstractType
+{
+       public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu',
+            ])
+            ->add('expiredAt', DateTimeType::class, [
+                'label' => 'Date d’expiration',
+                'required' => false,
+                'widget' => 'single_text',
+            ])
+            ->add('status')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
